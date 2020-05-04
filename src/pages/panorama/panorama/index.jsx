85e9847134721200a74d9a013a31b033d838fa41
Rusty-Trueno <1076061276@qq.com>
2020-05-04 22:15:27 +0800

    panorama update

--- src/pages/panorama/panorama/index.jsx
+++ src/pages/panorama/panorama/index.jsx
@@ -1,19 +1,29 @@
 import React,{Component} from 'react';
+import {Select,Modal} from 'antd';
 import Viewer from 'photo-sphere-viewer';
 import 'photo-sphere-viewer/dist/photo-sphere-viewer.css'
 import './index.styl'
-import pin1 from './icons/pin1.png'
+import axios from 'axios';
 
 
 export default class Panorama extends Component{
     constructor(props){
         super(props)
         this.state={
+            token:window.localStorage.getItem('token'),
             viewer:null,
             PSV:null,
             markers:[],
             clickMode:0,//marker的点击模式：0无效，1添加，2删除
             sceneInfo:null,
+            visible:false,
+            deviceList:[],
+            curDevice:null,
+            curClickInfo:null,
+            bindedDeviceList:[
+
+            ],
+            deviceDict:[],
         }
 
     }
@@ -23,6 +33,7 @@ export default class Panorama extends Component{
         this.setState({
             sceneInfo:sceneItem,
         })
+        this.getAllDevice();
         let viewer = document.getElementById('viewer');
         let panorama = this;
         let PSV = new Viewer({
@@ -84,10 +95,13 @@ export default class Panorama extends Component{
                 },
             ],
         });
-        console.log(PSV)
         this.setState({
             viewer:viewer,
             PSV:PSV,
+        })
+        //加载当前场景下的设备
+        PSV.on('panorama-load-progress',function(){
+            panorama.getBindedDevice(sceneItem,PSV);
         })
         //监听点击场景事件
         PSV.on('click',function(e){
@@ -97,50 +111,230 @@ export default class Panorama extends Component{
         PSV.on('select-marker',function(e){
             panorama.handleMarkerClick(e);
         })
+
     }
 
     handleMarkerClick=(e)=>{
         let{PSV,clickMode} = this.state;
         if(clickMode==2){
             //如果是设备删除
-            PSV.removeMarker(e);
-            this.setState({
-                clickMode:0,
-            })
+            this.deleteDevice(e,PSV);
+            
         }else{
             //如果只是查看
-            // PSV.gotoMarker(e,1000);
             alert("经度为：" + e.longitude + "\n" + "纬度为：" + e.latitude);
         }
+    }
+
+    //删除设备
+    deleteDevice=(marker,PSV)=>{
+        let {sceneInfo} = this.state;
+        let value={
+            deviceId:marker.id,
+            sceneId:sceneInfo.id,
+        };
+        console.log(value);
+        axios({
+            method: 'DELETE',
+            url: '/rdc/rdcScene/deleteBindedDevice',
+            headers: {
+                'deviceId': this.deviceId,
+                'Authorization':'Bearer '+this.state.token,
+            },
+            data:value
+        })
+        .then((res) => {
+            if(res && res.status === 200){
+                PSV.removeMarker(marker);
+                this.setState({
+                    clickMode:0,
+                })
+                alert("删除成功！")
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
     }
 
     addDevice=(e)=>{
         let{PSV,clickMode} = this.state;
         if(clickMode==1){
-            PSV.addMarker({
-                id: `#${Math.random()}`,
-                longitude: e.longitude,
-                latitude: e.latitude,
-                image: pin1,
-                width: 32,
-                height: 32,
-                anchor:'bottom center',
-                tooltip:{
-                    content:'deviceName',
-                    fontsize:'xx-large',
-                },
-            });
             this.setState({
-                clickMode:0,
+                curClickInfo:e,
+                visible:true,
             })
         }
     }
+
+
+    //获取当前场景下的全部设备
+    getBindedDevice=(sceneItem,PSV)=>{
+        axios({
+            method: 'GET',
+            url: '/rdc/rdcScene/getDeviceBySceneId/'+sceneItem.id,
+            headers: {
+                'deviceId': this.deviceId,
+                'Authorization':'Bearer '+this.state.token,
+            },
+        })
+        .then((res) => {
+            if(res && res.status === 200){
+                console.log(res.data.result)
+                this.setState({
+                    bindedDeviceList:res.data.result,
+                })
+                for(let i=0;i<res.data.result.length;i++){
+                    PSV.addMarker({
+                        id: res.data.result[i].deviceId,
+                        longitude: res.data.result[i].longitude,
+                        latitude: res.data.result[i].latitude,
+                        image: res.data.result[i].url,
+                        width: 40,
+                        height: 40,
+                        anchor:'bottom center',
+                        tooltip:{
+                            content:res.data.result[i].name,
+                            fontsize:'xx-large',
+                        },
+                    });
+                }
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    //获取全部设备
+
+    getAllDevice=()=>{
+        axios({
+            method: 'GET',
+            url: '/rdc/rdcDevice/getAllDevice',
+            headers: {
+                'deviceId': this.deviceId,
+                'Authorization':'Bearer '+this.state.token,
+            },
+        })
+        .then((res) => {
+            if(res && res.status === 200){
+                console.log(res.data.result)
+                this.setState({
+                    deviceList:res.data.result,
+                })
+                let deviceDict=[];
+                res.data.result.map((value,index)=>{
+                    deviceDict[value.id]=value;
+                })
+                this.setState({
+                    deviceDict:deviceDict,
+                })
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
+
+    //获取设备选项
+    getDeviceOption=()=>{
+        let deviceOption;
+        let {deviceList} = this.state;
+        console.log(deviceList)
+        deviceOption=deviceList&&deviceList.map((item, index) => (
+            <Select.Option key={item.id} value={item.id}> 
+                <div>
+                    {item.name}
+                    <img src={item.url} className='device-img'></img>
+                </div>
+            </Select.Option>
+        ));
+        return deviceOption;
+    }
+
+    //处理模态框的取消
+    handleCancel = e => {
+        console.log(e);
+        this.setState({
+            visible: false,
+            clickMode: 0,
+        });
+    };
+
+    handleDeviceSelect=(deviceId)=>{
+        let{deviceDict} = this.state;
+        this.setState({
+            curDevice:deviceDict[deviceId],
+        })
+    }
+
+    handleSceneBindDevice=()=>{
+        let {PSV,curDevice,sceneInfo,curClickInfo} = this.state;
+        console.log(curDevice);
+        console.log(sceneInfo)
+        let value={
+            deviceId:curDevice.id,
+            sceneId:sceneInfo.id,
+            longitude:curClickInfo.longitude,
+            latitude:curClickInfo.latitude,
+        };
+        console.log(value)
+        console.log(curClickInfo)
+        axios({
+            method: 'POST',
+            url: '/rdc/rdcScene/sceneBindDevice',
+            headers: {
+                'deviceId': this.deviceId,
+                'Authorization':'Bearer '+this.state.token,
+            },
+            data:value
+        })
+        .then((res) => {
+            if(res && res.status === 200){
+                console.log(res.data.result)
+                alert("设备添加成功")
+                this.setState({
+                    clickMode:0,
+                })
+                this.props.history.push({
+                    pathname:'/cbd/panorama/panorama',
+                    state:{
+                        sceneItem:this.state.sceneInfo
+                    },
+                })
+            }
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+        this.setState({
+            visible:false,
+            clickMode:0,
+        })
+    }
+    
 
     render(){
         
         return(
             <div>
                 <div id='viewer' ref='viewer' className='viewer'></div>
+                <Modal
+                    title="设备选择"
+                    visible={this.state.visible}
+                    onOk={()=>{this.handleSceneBindDevice()}}
+                    onCancel={this.handleCancel}
+                >
+                    <Select
+                        placeholder="请选择设备"
+                        className="device-select"
+                        onChange={(value) => { this.handleDeviceSelect(value) }}
+                        allowClear
+                    >
+                        {this.getDeviceOption()}
+                    </Select>
+                </Modal>
             </div>
             
         ) 
