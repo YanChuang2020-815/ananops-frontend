7cde7f56d4da1688db24b08210c236e07f3495b2
SongOf <lisongsong2015@bupt.edu.cn>
2020-05-08 23:28:13 +0800

    全部设备

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,9 +2,9 @@
 //const target = 'http://39.104.189.84:30300';  //阿里云IP
 // const target = 'http://10.128.175.8:8086'
 //const target = 'http://10.112.217.199:7979';
-// const target = 'https://www.ananops.com/api/';
+const target = 'https://www.ananops.com/api/';
 // const target='http://www.ananops.com:29995';
-const target = 'http://10.112.196.254:29995';
+//const target = 'http://10.112.196.254:29995';
 
 const gantch = 'https://smart.gantch.cn/api/v1/'
 
