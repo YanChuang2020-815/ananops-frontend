72c29662cd6bf1aea5973fb6311b999f76b87d81
Rusty-Trueno <1076061276@qq.com>
2020-07-07 10:55:49 +0800

    增加边缘设备查看

--- src/pages/edgeDevice/device/index.jsx
+++ src/pages/edgeDevice/device/index.jsx
@@ -1,5 +1,6 @@
 import React,{Component} from 'react';
-import {Card,Popconfirm} from 'antd';
+import {Table, Button, Card, Select, Input, Icon, Modal, message, Alert, Tag,Space} from 'antd';
+import LinkButton from '../../../components/link-button'
 import axios from 'axios';
 
 export default class Device extends Component{
@@ -17,12 +18,127 @@ export default class Device extends Component{
     
   }
 
-  
+  initColumns = () => {
+    this.columns = [
+      {
+        title:'序号',
+        width: 50,
+        fixed:'left',
+        dataIndex: 'id',
+        render:(text,record,index)=> `${index+1}`,
+      },
+      {
+        title:'名称',
+        dataIndex:'metadata.name',
+        width:70
+      },
+      {
+        title:'设备模型',
+        dataIndex:'metadata.labels.model',
+        width:70,
+      },
+      {
+        title:'API版本',
+        dataIndex:'apiVersion',
+        width:150,
+      },
+      {
+        title:'资源类型',
+        dataIndex:'kind',
+        width:70,
+      },
+      {
+        title:'操作',
+        width:280,
+        fixed:'right',
+        render: (device) => { 
+            return (
+                <span>
+                  <LinkButton onClick={() => alert("修改")}>修改</LinkButton>|
+                  <LinkButton onClick={() => alert("详情")}>详情</LinkButton>|
+                  <LinkButton onClick={() => alert("删除")}>删除</LinkButton>
+                </span>
+            )
+          }
+      }
+    ]
+  }
 
+  componentWillMount() {
+    this.initColumns()
+  }
 
   render(){
+    const mockData = [
+      {
+        apiVersion: 'devices.kubeedge.io/v1alpha1',
+        kind: 'Device',
+        metadata: {
+          name: 'led-light-instance-01',
+          labels: {
+            description: 'LEDLight',
+            model: 'led-light'
+          }
+        },
+        spec: {
+          deviceModelRef: {
+            name: 'led-light'
+          },
+          nodeSelector: {
+            nodeSelectorTerms: [
+              {
+                matchExpressions: [
+                  {
+                    key:'',
+                    operator: 'In',
+                    values: [
+                      'edge-node1'
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        },
+        status: {
+          twins: [
+            {
+              propertyName: 'power-status',
+              desired: {
+                metadata: {
+                  type: 'string'
+                },
+                value: 'OFF'
+              }
+            }
+          ]
+        }
+      }
+    ]
+
+    const extra = (
+      <span>
+        <Button type="primary" style={{marginRight:'15px'}} onClick={() => alert("添加设备")}><Icon type="plus"/>添加设备</Button>
+      </span>
+
+    )
     return (
-      "边缘设备管理"
+      <Card extra={extra}>
+        <Table
+          bordered
+          //loading={loading}
+          rowKey="id"
+          size='small'
+          dataSource={mockData}
+          columns={this.columns}
+          pagination={{
+            current:this.pageNum,
+            defaultPageSize:10,
+            showQuickJumper:true,
+            total:0,
+          }}
+        />
+      </Card>
     )
   }
 }
